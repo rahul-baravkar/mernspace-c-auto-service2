5332e13bc9c05af321876ec4f43ae3498ed6ea0d
rahulbaravkar-new-account <baravkarrahul400@gmail.com>
2025-12-27 16:46:26 +0530

    add express server

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+   tseslint.configs.recommendedTypeChecked,
+   {
+    ignores:['dist', 'node_modules' , 'eslint.config.mjs'],
+
+   },
+  {
+    
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir:import.meta.dirname,
+      },
+    },
+    rules:{
+      'dot-notation':'off'
+    }
+  },
+);
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,17 @@
-function Welcome(name: string) {
-  console.log('Welcome' + name);
-}
+import app from "./app.js";
+import { Config } from "./config/index.js";
 
-Welcome('rahul');
+const startServer = () => {
+  const { PORT } = Config;
+
+  const server = app.listen(PORT, () => {
+    console.log(`🚀 Auth service running on port ${PORT}`);
+  });
+
+  server.on("error", (error) => {
+    console.error("Server failed to start:", error);
+    process.exit(1);
+  });
+};
+
+startServer();
